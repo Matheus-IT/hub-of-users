d61e51ab44cd53e98515e1f9de7ce1e961a897e4
Matheus da Costa da Silva <matheuscosta.tech@gmail.com>
2020-12-21 16:09:48 -0300

    Configuration to setup the 'signals.py'

---
+++ users_hub/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'users_hub.apps.ExampleConfig'
--- users_hub/apps.py
+++ users_hub/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 
 class UsersHubConfig(AppConfig):
     name = 'users_hub'
+
+    def ready(self):
+    	import users_hub.signals
