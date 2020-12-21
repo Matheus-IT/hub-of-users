ed9e2d7b311760a7cc8792935fd067595148ac00
Matheus da Costa da Silva <matheuscosta.tech@gmail.com>
2020-12-21 15:55:12 -0300

    Simple model for keeping track of logged_in_users

--- users_hub/models.py
+++ users_hub/models.py
@@ -1,5 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+
 
 class User(AbstractUser):
 	pass
+
+
+class LoggedInUser(models.Model):
+	""" Create a LoggedInUser instance when a user logs in, and the app will delete the instance
+		when the user logs out """
+	user = models.OneToOneField(
+		settings.AUTH_USER_MODEL, 
+		on_delete=models.CASCADE, 
+		related_name='logged_in_user'
+	)
