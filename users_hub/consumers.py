acad9cfd69dca87dfb6b9eb28faed9bfe8e76905
Matheus da Costa da Silva <matheuscosta.tech@gmail.com>
2020-12-21 18:58:05 -0300

    New consumer to handle hub of users

--- users_hub/consumers.py
+++ users_hub/consumers.py
@@ -15,12 +15,38 @@ class UsersHubConsumer(AsyncWebsocketConsumer):
 		user_info = json.dumps({
 			'id': self.scope['user'].id,
 			'email': self.scope['user'].email,
+			'status_online': True
 		})
 
-		await self.send(text_data=user_info)
+		await self.channel_layer.group_send(
+			self.GROUP_NAME,
+			{
+				'type': 'send_user_info_to_client',
+				'user_info': user_info
+			}
+		)
 	
 	async def disconnect(self, close_code):
 		await self.channel_layer.group_discard(
 			self.GROUP_NAME,
 			self.channel_name
 		)
+
+		user_info = json.dumps({
+			'id': self.scope['user'].id,
+			'email': self.scope['user'].email,
+			'status_online': False
+		})
+
+		await self.channel_layer.group_send(
+			self.GROUP_NAME,
+			{
+				'type': 'send_user_info_to_client',
+				'user_info': user_info
+			}
+		)
+
+	async def send_user_info_to_client(self, event):
+		await self.send(text_data=json.dumps({
+			'userInfo': event['user_info']
+		}))
